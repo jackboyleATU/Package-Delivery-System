a3c72109fb831a8f76efe3cd71b41b113ca14217
FrancescaP933 <FrancescaP933@users.noreply.github.com>
2026-03-10 21:29:20 +0000

    basic sign-in system

---
+++ PackageDeliverySystem/Pages/Logout.cshtml.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PackageDeliverySystem.Pages
+{
+    public class LogoutModel : PageModel
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public LogoutModel(SignInManager<IdentityUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPostLogoutAsync()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToPage("Login");
+        }
+
+        public IActionResult OnPostNoLogout()
+        {
+            return RedirectToPage("Index");
+        }
+    }
+}
---
+++ PackageDeliverySystem/Pages/PageViewModels/Login.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PackageDeliverySystem.Pages.PageViewModels
+{
+    public class Login
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+
+        public string EmailAddress { get; set; }
+        [Required]
+        [DataType(DataType.Password)] public string Password { get; set; } = string.Empty;
+
+        public bool RememberMe { get; set; }
+    }
+}
